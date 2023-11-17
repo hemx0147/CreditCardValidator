de4b4e4ade4aa7a0109dc89edd06c2f71942be74
hemx0147 <rnx781@gmail.com>
2023-11-17 12:11:07 +0700

    add no-UI option & command line args parsing

--- CCValidator/main.cpp
+++ CCValidator/main.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
 #include "CCValidator.h"
 #include "ConsoleUi.h"
+#include <vector>
 
 
-int main()
+int main(int argc, char *argv[])
 {
+    // put all command line params in vector, check if correct param is given and use its value as card number.
+    // -c option checks given number without UI and returns result as success/error program exit code.
+    std::string cardNum = "";
+    std::vector<std::string> args(argv + 1, argv + argc);
+    if (args[0] == "-c")
+    {
+        if (args.size() > 1)
+        {
+            cardNum = args[1];
+        }
+        return !ccval::cardNumIsValid(cardNum);
+    }
+
+    // program call with invalid or without parameters use console UI version
     ConsoleUi ui;
     ui.drawBanner();
-
-    std::string cardNum;
     bool isValid = false;
     while (true)
     {
