ec8bd69c091954f3268752e4987617b9ab285d32
hemx0147 <rnx781@gmail.com>
2023-11-17 13:02:29 +0700

    fix command line args vector out of range error

--- CCValConsole/main.cpp
+++ CCValConsole/main.cpp
@@ -6,17 +6,20 @@
 
 int main(int argc, char *argv[])
 {
-    // put all command line params in vector, check if correct param is given and use its value as card number.
-    // -c option checks given number without UI and returns result as success/error program exit code.
     std::string cardNum = "";
-    std::vector<std::string> args(argv + 1, argv + argc);
-    if (args[0] == "-c")
+    if (argc > 1)
     {
-        if (args.size() > 1)
+        // put all command line params in vector, check if correct param is given and use its value as card number.
+        // -c option checks given number without UI and returns result as success/error program exit code.
+        std::vector<std::string> args(argv + 1, argv + argc);
+        if (args[0] == "-c")
         {
-            cardNum = args[1];
+            if (args.size() > 1)
+            {
+                cardNum = args[1];
+            }
+            return !ccval::cardNumIsValid(cardNum);
         }
-        return !ccval::cardNumIsValid(cardNum);
     }
 
     // program call with invalid or without parameters use console UI version
